a065706da1c41c622e5a30b432baba0540e0a866
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2023-05-29 17:56:00 +0800

    hw4p2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Script outline to install and build kernel.
+# Author: Siddhant Jajoo.
 
 set -e
 set -u
@@ -32,19 +34,34 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+	# Fix for:
+	# scripts/dtc/dtc-parser.tab.o:(.bss+0x20): multiple definition of `yylloc';
+	# scripts/dtc/dtc-lexer.lex.o:(.bss+0x0): first defined here
+	sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/g' scripts/dtc/dtc-lexer.l
 
+    # TODO: Add your kernel build steps here
+	# clean
+	echo "Cleaning kernel build mrproper"
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	# defconfig "virt" is default no arg
+	echo "Setting up default config for target virt(default)"
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	# build the kernel 
+	echo "Building kernel..."
+	make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+	# Build kernel mods
+	echo "Building kernel modules..."
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+	# Build device tree
+	echo "Bulding device tree..."
+	make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+cd $OUTDIR
+cp -a linux-stable/arch/arm64/boot/Image ./
 
 echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
@@ -52,12 +69,16 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir rootfs
-cd rootfs
-mkdir -p bin dev etc home lib lib64 proc sbin tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
+ROOTFS_DIR=${OUTDIR}/rootfs
 
+# Create the empty rootfs staging
+mkdir ${ROOTFS_DIR}
+
+# Create the Filesystem Hierarchy Standard (HFS) dirs
+cd rootfs
+mkdir bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin
+mkdir -p /var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -66,65 +87,100 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-
-    make distclean
-    make defconfig
-
+	make distclean
+	make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs  ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
-cd "${OUTDIR}"/rootfs
 
-echo "Library dependencies"
+# TODO: Make and install busybox
+echo -e "Make and install busybox"
+sudo env "PATH=$PATH" make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${ROOTFS_DIR} install
+
+echo "Copying busybox library dependencies to rootfs"
+cd ${ROOTFS_DIR}
+
+# Cross-compile sysroot dir
+CCSYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+# Show dependencies
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
+# Interpreter / LIBS dependecies
+REQ_INTRP=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | awk '{ gsub(/\[|\]/,"",$NF); print $NF}')
+REQ_LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | awk '{ gsub(/\[|\]/,"",$NF); print $NF}')
+
 # TODO: Add library dependencies to rootfs
-cd "${OUTDIR}/rootfs"
+for intrp in $REQ_INTRP
+do
+    echo "intrp: $intrp"
+	intrp=$(basename ${intrp})
+	LIB_SRC=$(find ${CCSYSROOT} -name $intrp)
+	LIB_TGT=$(realpath --no-symlinks --relative-to=$CCSYSROOT $LIB_SRC)
+	
+	echo "Copy $LIB_SRC to $LIB_TGT"
+	cp -a $LIB_SRC $LIB_TGT
+	
+    if [ -L $LIB_TGT ]; then
+		LNK_SRC=$(readlink -f $LIB_SRC)
+	    LNK_TGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+		echo "Copy link: $LNK_SRC to $LNK_TGT"
+		cp -a $LNK_SRC $LNK_TGT
+    fi
+done
 
-cp "$(${CROSS_COMPILE}gcc -print-sysroot)/lib/ld-linux-aarch64.so.1" "${OUTDIR}/rootfs/lib"
 
-cp "$(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libm.so.6" "${OUTDIR}/rootfs/lib64"
-cp "$(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libc.so.6" "${OUTDIR}/rootfs/lib64"
-cp "$(${CROSS_COMPILE}gcc -print-sysroot)/lib64/libresolv.so.2" "${OUTDIR}/rootfs/lib64"
+for lib in $REQ_LIBS
+do
+    lib=$(basename ${lib})
+	LIB_SRC=$(find ${CCSYSROOT} -name $lib)
+	LIB_TGT=$(realpath --no-symlinks --relative-to=$CCSYSROOT $LIB_SRC)
+	echo "Copy $LIB_SRC to $LIB_TGT"
+	cp -a $LIB_SRC $LIB_TGT
+
+	LNKTGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+	
+    # If src is link, copy link target locally
+    if [ -L $LIB_TGT ]; then
+		LNK_SRC=$(readlink -f $LIB_SRC)
+	    LNK_TGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+		echo "Copy link: $LNK_SRC to $LNK_TGT"
+		cp -a $LNK_SRC $LNK_TGT
+    fi
+done
+cd ${ROOTFS_DIR}
 
 # TODO: Make device nodes
-cd "${OUTDIR}/rootfs"
-mkdir -p dev
-
+echo -e "Creating device nodes"
 sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1
+sudo mknod -m 666 dev/char0 c 5 2
 
 # TODO: Clean and build the writer utility
-cd $FINDER_APP_DIR
-export CROSS_COMPILE=aarch64-none-linux-gnu-
-make clean
-make CROSS_COMPILE=${CROSS_COMPILE}gcc
+echo -e "Cleaning and building writer utility"
+cd ${FINDER_APP_DIR}
+make CROSS_COMPILE=${CROSS_COMPILE} clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
-cp -r "${FINDER_APP_DIR}/../conf" "${OUTDIR}/rootfs/home"
-
-cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home"
-
-cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home"
-
-cp "${FINDER_APP_DIR}/finder-test.sh" "${OUTDIR}/rootfs/home"
-
-cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home"
+# Copy your finder.sh, conf/username.txt and (modified as described in step 1 above)
+# finder-test.sh scripts from Assignment 2 into the outdir/rootfs/home directory 
+cp -a $FINDER_APP_DIR/writer $ROOTFS_DIR/home/
+echo -e "Copy finder app dir to rootfs/home"
+cp -a $FINDER_APP_DIR/finder.sh $ROOTFS_DIR/home/
+mkdir $ROOTFS_DIR/home/conf
+cp -a $FINDER_APP_DIR/conf/username.txt $ROOTFS_DIR/home/conf/
+cp -a $FINDER_APP_DIR/finder-test.sh $ROOTFS_DIR/home/
+cp -a $FINDER_APP_DIR/autorun-qemu.sh $ROOTFS_DIR/home/
 
 # TODO: Chown the root directory
-
-cd "${OUTDIR}/rootfs"
-
-sudo chown -R root:root *
+echo -e "Chaning owner of rootfs dir to root"
+sudo chown -R root:root ${ROOTFS_DIR}
 
 # TODO: Create initramfs.cpio.gz
-find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-
-gzip -f ${OUTDIR}/initramfs.cpio
+cd $ROOTFS_DIR
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
