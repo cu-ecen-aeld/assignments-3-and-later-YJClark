c633b628675c0476b976f09c2382d53530a5a389
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2023-05-29 17:18:12 +0800

    Update dependencies.sh

--- finder-app/dependencies.sh
+++ finder-app/dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Dependency installation script for kernel build.
-# Author: Siddhant Jajoo.
-
-
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y u-boot-tools
 sudo apt-get install -y qemu
