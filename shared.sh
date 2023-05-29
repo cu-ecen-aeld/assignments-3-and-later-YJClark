31f829b36070085f6b17b6ead3811ad3581e22e3
miracul9228 <134514965+miracul9228@users.noreply.github.com>
2023-05-29 19:54:51 +0800

    Update shared.sh

--- shared.sh
+++ shared.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Shared definitions for buildroot scripts
-
 # The defconfig from the buildroot directory we use for qemu builds
 QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
 # The place we store customizations to the qemu configuration
